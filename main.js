4fff7475a6fb5843c515e0c450e9d3eb8d33f779
Natanael Haiduk <92743826+nhaiduk@users.noreply.github.com>
2021-11-01 00:39:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+//camelCase
+//PascalCase
+//snake_case
+
+const LinksSocialMedia = {
+  github: 'nhaiduk',
+  youtube: 'haiduk007',
+  facebook: 'natanael.haiduk',
+  instagram: 'natanaelhaiduk',
+  twitter: ''
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+
+    //alert(li.children[0].href)
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
